651a9d227bd073e546aa9c9c2f7eb1aa0b49e3d0
Eduer Pallares <eduer.pallares@zalvadora.com>
2020-12-09 07:28:44 -0500

    controladores

---
+++ application/controllers/codigofacilito.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class CodigoFacilito extends CI_Controller {
+  function __construct()  {
+    parent::__construct();
+  }
+
+  public function index() {
+    $data = [
+      'title' => 'Codigo Facilito',
+      'firstname' => 'Eduer',
+      'lastname' => 'Pallares Jiménez'
+    ];
+
+    $this->load->view('codigofacilito/bienvenido', $data);
+  }
+
+  public function soyExepcional() {
+    $data = [
+      'title' => 'Codigo Facilito',
+      'firstname' => 'Eduer',
+      'lastname' => 'Pallares Jiménez'
+    ];
+    $this->load->view('layouts/header', $data);
+    $this->load->view('codigofacilito/soygenial', $data);
+    $this->load->view('layouts/footer', $data);
+  }
+}
---
+++ application/views/codigofacilito/bienvenido.php
@@ -0,0 +1,22 @@
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <?= $title ?>
+    </div>
+    <div class="col-sm">
+      <?= $firstname ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+      <?= $lastname ?>
+    </div>
+    <div class="col-sm">
+      Developer
+    </div>
+    <div class="col-sm">
+      Expert
+    </div>
+  </div>
+</div>
+
