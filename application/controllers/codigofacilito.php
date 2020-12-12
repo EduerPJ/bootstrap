9672e9b76e81ca0d30e1407a60be77c32896c06f
Eduer Pallares <eduer.pallares@zalvadora.com>
2020-12-12 08:38:18 -0500

    formularios

--- application/controllers/codigofacilito.php
+++ application/controllers/codigofacilito.php
@@ -3,12 +3,11 @@
 class CodigoFacilito extends CI_Controller {
   function __construct()  {
     parent::__construct();
-    $this->load->helper('mihelper');
+    $this->load->helper('form');
+    $this->load->library('Menu', array('Inicio', 'Contacto', 'Cursos'));
   }
 
   public function index() {
-    $this->load->library('Menu', array('Inicio', 'Contacto', 'Cursos'));
-
     $data = [
       'mi_menu' => $this->menu->construirMenu(),
       'title' => 'Codigo Facilito',
@@ -20,7 +19,6 @@ class CodigoFacilito extends CI_Controller {
   }
 
   public function soyExepcional() {
-    $this->load->library('Menu', array('Inicio', 'Contacto', 'Cursos'));
     $data = [
       'mi_menu' => $this->menu->construirMenu(),
       'title' => 'Codigo Facilito',
@@ -29,6 +27,19 @@ class CodigoFacilito extends CI_Controller {
     ];
     $this->load->view('layouts/header', $data);
     $this->load->view('codigofacilito/soygenial', $data);
+    $this->load->view('layouts/footer', $data);
+  }
+
+  public function formulario(){
+    $data = [
+      'mi_menu' => $this->menu->construirMenu(),
+      'title' => 'Codigo Facilito',
+      'firstname' => 'Eduer',
+      'lastname' => 'Pallares Jiménez'
+    ];
+
+    $this->load->view('layouts/header', $data);
+    $this->load->view('codigofacilito/formulario', $data);
     $this->load->view('layouts/footer', $data);
   }
 }
